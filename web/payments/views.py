f2298c8435b95fdbf7230d80a284eeb1fd3d1c93
Michał Pielak <mpielak@okcode.eu>
2021-09-20 21:43:59 +0200

    save

--- web/payments/views.py
+++ web/payments/views.py
@@ -39,31 +39,31 @@ class StripeWebhookView(APIView):
     permission_classes = []
 
     def post(self, request, *args, **kwargs):
-        payload = request.body
-        sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-        event = None
+        # payload = request.body
+        # sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+        # event = None
 
-        try:
-            event = stripe.Webhook.construct_event(
-            payload, sig_header, endpoint_secret
-            )
-        except ValueError as e:
-            # Invalid payload
-            return HttpResponse(status=400)
-        except stripe.error.SignatureVerificationError as e:
-            # Invalid signature
-            return HttpResponse(status=400)
+        # try:
+        #     event = stripe.Webhook.construct_event(
+        #     payload, sig_header, endpoint_secret
+        #     )
+        # except ValueError as e:
+        #     # Invalid payload
+        #     return HttpResponse(status=400)
+        # except stripe.error.SignatureVerificationError as e:
+        #     # Invalid signature
+        #     return HttpResponse(status=400)
 
-        # Handle the event
-        if event.type == 'payment_intent.succeeded':
-            payment_intent = event.data.object # contains a stripe.PaymentIntent
-            print('PaymentIntent was successful!')
-        elif event.type == 'payment_method.attached':
-            payment_method = event.data.object # contains a stripe.PaymentMethod
-            print('PaymentMethod was attached to a Customer!')
-        # ... handle other event types
-        else:
-            print('Unhandled event type {}'.format(event.type))
+        # # Handle the event
+        # if event.type == 'payment_intent.succeeded':
+        #     payment_intent = event.data.object # contains a stripe.PaymentIntent
+        #     print('PaymentIntent was successful!')
+        # elif event.type == 'payment_method.attached':
+        #     payment_method = event.data.object # contains a stripe.PaymentMethod
+        #     print('PaymentMethod was attached to a Customer!')
+        # # ... handle other event types
+        # else:
+        #     print('Unhandled event type {}'.format(event.type))
 
         return HttpResponse(status=200)
 
