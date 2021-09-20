5b16d1f34229996a5820b14c4ba8a52d6e6640f6
Michał Pielak <mpielak@okcode.eu>
2021-09-20 21:35:20 +0200

    save

--- web/payments/views.py
+++ web/payments/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render
 from django.views import View
 from django.views.decorators.csrf import csrf_protect
 from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
+
 
 import stripe
 import json
@@ -30,35 +32,41 @@ class PaymentIntentView(View):
         }
         return render(request, "payments/payment_intent.html", ctx)
 
+endpoint_secret =  settings.STRIPE_ENDPOINT_SECRET
+
 @csrf_protect
-class StripeWebhookView(views):
-    def post(self, request):
+class StripeWebhookView(APIView):
+    permission_classes = []
+
+    def post(self, request, *args, **kwargs):
         payload = request.body
+        sig_header = request.META['HTTP_STRIPE_SIGNATURE']
         event = None
 
         try:
-            event = stripe.Event.construct_from(
-            json.loads(payload), stripe.api_key
+            event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
             )
         except ValueError as e:
             # Invalid payload
+            return HttpResponse(status=400)
+        except stripe.error.SignatureVerificationError as e:
+            # Invalid signature
             return HttpResponse(status=400)
 
         # Handle the event
         if event.type == 'payment_intent.succeeded':
             payment_intent = event.data.object # contains a stripe.PaymentIntent
-           
-            # Then define and call a method to handle the successful payment intent.
-            # handle_payment_intent_succeeded(payment_intent)
+            print('PaymentIntent was successful!')
         elif event.type == 'payment_method.attached':
             payment_method = event.data.object # contains a stripe.PaymentMethod
-            # Then define and call a method to handle the successful attachment of a PaymentMethod.
-            # handle_payment_method_attached(payment_method)
+            print('PaymentMethod was attached to a Customer!')
         # ... handle other event types
         else:
             print('Unhandled event type {}'.format(event.type))
 
         return HttpResponse(status=200)
 
+
 payment_intent = PaymentIntentView.as_view()
-stripe_webhook = StripeWebhookView.as_view()
+stripe_webhook = StripeWebhookView
