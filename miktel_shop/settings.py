63f68e75e829c1d2fa680c523b0ba7286fba54d3
Michał Pielak <toja@example.com>
2022-03-08 06:50:33 +0100

    on load

--- miktel_shop/settings.py
+++ miktel_shop/settings.py
@@ -146,6 +146,7 @@ STATICFILES_FINDERS = (
     "compressor.finders.CompressorFinder",
 )
 
+
 WSGI_APPLICATION = "miktel_shop.wsgi.application"
 
 
@@ -193,7 +194,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = "static"
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (os.path.join(SITE_ROOT, "static/"),)
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
