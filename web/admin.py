7635ebdacd8ac13a83310fcf6a2ec7f9721ba765
Michał Pielak <toja@example.com>
2022-04-18 10:54:13 +0200

    keys

--- web/admin.py
+++ web/admin.py
@@ -11,53 +11,55 @@ from weasyprint import HTML
 from web.models import *
 from web.models.orders import IndexAlfaStamp
 
-@admin.action(description='Utwórz profil')
+
+@admin.action(description="Utwórz profil")
 def make_active(modeladmin, request, queryset):
     for user in queryset:
         token = ActivateToken.objects.create(
-                    user=user,
-                    activation_token=str(int(str(uuid4()).split("-")[0], 16)),
-                )
+            user=user,
+            activation_token=str(int(str(uuid4()).split("-")[0], 16)),
+        )
         user.is_active = True
         user.email = user.username
         user.save()
 
+
 class CustomUserAdmin(UserAdmin):
     list_display = [f.name for f in User._meta.fields]
     search_fields = ("username",)
-    list_display_links = ('username',)
-    exclude = ['pk']
+    list_display_links = ("id", "username",)
     actions = [make_active]
+
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
 
+
 @admin.register(Articles)
 class ArticlesAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Articles._meta.fields]
-    list_display_links = ('title',)
+    list_display_links = ("id", "title",)
     list_filter = ("category",)
     search_fields = ("title",)
-    exclude = ['pk']
+
 
 @admin.register(ActivateToken)
 class ActivateTokenAdmin(admin.ModelAdmin):
     list_display = [f.name for f in ActivateToken._meta.fields]
     search_fields = ("user",)
-    list_display_links = ('activation_token',)
-    exclude = ['pk']
+    list_display_links = ("id",  "activation_token",)
+
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Profile._meta.fields]
     list_filter = ("company",)
-    list_display_links = ('user',)
+    list_display_links = ("id",  "user",)
     search_fields = (
         "last_name",
         "username",
         "nip_number",
     )
-    exclude = ['pk']
 
 
 @admin.register(Address)
@@ -68,20 +70,20 @@ class AddressAdmin(admin.ModelAdmin):
         # 'worker_position',
     )
     search_fields = ("user_id",)
-    list_display_links = ('user',)
-    exclude = ['pk']
+    list_display_links = ("id",  "user",)
+
 
 @admin.register(PayMethod)
 class PayMethonAdmin(admin.ModelAdmin):
     list_display = [f.name for f in PayMethod._meta.fields]
-    list_display_links = ('name',)
-    exclude = ['pk']
+    list_display_links = ("id",  "name",)
+
 
 @admin.register(DeliveryMethod)
 class DeliveryMethodAdmin(admin.ModelAdmin):
     list_display = [f.name for f in DeliveryMethod._meta.fields]
-    list_display_links = ('name',)
-    exclude = ['pk']
+    list_display_links = ("id",  "name",)
+
 
 @admin.register(Orders)
 class OrdersAdmin(admin.ModelAdmin):
@@ -90,14 +92,15 @@ class OrdersAdmin(admin.ModelAdmin):
         "store",
         "pay_method",
     )
-    list_display_links = ('number',)
+    list_display_links = ("id",  "number",)
     search_fields = ("number",)
-    exclude = ['pk']
+
 
 @admin.register(ProductCopy)
 class ProductCopyAdmin(admin.ModelAdmin):
     list_display = [f.name for f in ProductCopy._meta.fields]
-    exclude = ["order", "pk"]
+    exclude = ["order",]
+
 
 # Register your models here.
 
@@ -105,26 +108,27 @@ class ProductCopyAdmin(admin.ModelAdmin):
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Store._meta.fields]
-    exclude = ['pk']
-    list_display_links = ('name',)
+    list_display_links = ("id", "name",)
+
 
 @admin.register(Colors)
 class ColorsAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Colors._meta.fields]
-    exclude = ['pk']
-    list_display_links = ('name',)
+    list_display_links = ("id",  "name",)
+
 
 @admin.register(Vat)
 class VatAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Vat._meta.fields]
-    exclude = ['pk']
-    list_display_links = ('name',)
+    list_display_links = ("id",  "name",)
+
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Brand._meta.fields]
-    exclude = ['pk']
-    list_display_links = ('name',)
+
+    list_display_links = ("id",  "name",)
+
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
@@ -135,53 +139,53 @@ class ProductsAdmin(admin.ModelAdmin):
         "is_recommended",
         "is_news",
         "is_promo",
-        "sub_category_type"
-
+        "sub_category_type",
     )
-    search_fields = ("name", "pk", )
-    list_display_links = ('name',)
-    exclude = ['pk']
+    search_fields = (
+        "name",
+        "id",
+    )
+    list_display_links = ("id",  "name",)
+
 
 @admin.register(Size)
 class SizeAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Size._meta.fields]
     # list_filter = ('sub_category', )
     search_fields = ("name",)
-    list_display_links = ('name',)
-    exclude = ['pk']
+    list_display_links = ("id",  "name",)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Category._meta.fields]
     search_fields = ("name",)
-    list_display_links = ('name',)
-    exclude = ['pk']
+    list_display_links = ("id",  "name",)
+
 
 @admin.register(SubCategory)
 class SubCategoryAdmin(admin.ModelAdmin):
     list_display = [f.name for f in SubCategory._meta.fields]
     search_fields = ("name",)
-    list_display_links = ('name',)
-    exclude = ['pk']
+    list_display_links = ("id",  "name",)
 
 
 @admin.register(SubCategoryType)
 class SubCategoryTypeAdmin(admin.ModelAdmin):
     list_display = [f.name for f in SubCategoryType._meta.fields]
-    list_display_links = ('name',)
+    list_display_links = ("id",  "name",)
     search_fields = ("name",)
-    exclude = ['pk']
+
 
 @admin.register(Images)
 class ImagesAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Images._meta.fields]
-    list_display_links = ('image',)
-    exclude = ['pk']
+    list_display_links = ("id",  "image",)
     # list_filter = ('post', )
     # search_fields = ('post', )
 
 
-@admin.action(description='Utwórz fakturę')
+@admin.action(description="Utwórz fakturę")
 def create_invoice(modeladmin, request, queryset):
     for invoice in queryset:
         try:
@@ -200,32 +204,39 @@ def create_invoice(modeladmin, request, queryset):
         else:
             html.write_pdf(target=settings.MEDIA_ROOT + str(invoice.pdf))
 
+
 @admin.register(Invoices)
 class InvoicesAdmin(admin.ModelAdmin):
     # list_display = [f.name for f in Invoices._meta.fields]
-    list_display = ('number', 'created_time', 'pdf')
-    list_display_links = ('number',)
-    exclude = ['pk']
+    list_display = ("id", "number", "created_time", "pdf")
+    list_display_links = ("id",  "number",)
     # list_filter = ('post', )
     # search_fields = ('post', )
-    actions = [create_invoice,]
+    actions = [
+        create_invoice,
+    ]
 
 
 @admin.register(IndexAlfaStamp)
 class IndexAlfaStampAdmin(admin.ModelAdmin):
     # list_display = [f.name for f in Citys._meta.fields]
-    list_display = ('name', 'city_one', 'city_two',
-                    'city_three', 'city_four', 'city_five', )
-    list_display_links = ('name',)
-    exclude = ['pk']
-    search_fields = ('name', )
+    list_display = ("id", 
+        "name",
+        "city_one",
+        "city_two",
+        "city_three",
+        "city_four",
+        "city_five",
+    )
+    list_display_links = ("id",  "name",)
+
+    search_fields = ("name",)
 
 
 @admin.register(Citys)
 class CitysAdmin(admin.ModelAdmin):
     # list_display = [f.name for f in Citys._meta.fields]
-    list_display = ('name', 'rybna_area')
-    list_display_links = ('name',)
-    exclude = ['pk']
-    list_filter = ('rybna_area', )
-    search_fields = ('name', )
+    list_display = ("id", "name", "rybna_area")
+    list_display_links = ("id",  "name",)
+    list_filter = ("rybna_area",)
+    search_fields = ("name",)
