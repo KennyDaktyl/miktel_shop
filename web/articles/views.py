4c63ea8149d331f9ca66f7b0495304795c19bdd6
mpielak <mpielak@grupazpr.pl>
2022-04-29 10:59:36 +0200

    breadcrumbs

--- web/articles/views.py
+++ web/articles/views.py
@@ -3,6 +3,7 @@ from django.views.generic.base import RedirectView
 from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404, redirect
 from web.models.articles import Articles
+from web.models.products import Category
 
 
 class ArticleDetails(DetailView):
@@ -18,9 +19,21 @@ class ArticleList(ListView):
     model = Articles
     paginate_by = 30
     template_name = "articles/articles_list.html"
+    
+    def get_queryset(self):
+        articles = Articles.objects.all()
+        if self.request.GET.get('typ'):
+            self.category = get_object_or_404(
+            Category, pk=self.request.GET.get('typ')
+            )
+            return articles.filter(category=int(self.request.GET.get('typ')))
+        return articles
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        categories_pk = self.object_list.values_list(
+            'category', flat=True).distinct().order_by()
+        context["categories"] = Articles.objects.filter(pk__in=[categories_pk])
         return context
 
 
