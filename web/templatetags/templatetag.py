cbd70a8fe0fccc12936b94a0342dd4219fd772a3
Michał Pielak <toja@example.com>
2022-06-05 14:01:28 +0200

    citys

--- web/templatetags/templatetag.py
+++ web/templatetags/templatetag.py
@@ -1,3 +1,4 @@
+from operator import le
 from django import template
 import datetime
 
@@ -25,3 +26,20 @@ def replace_decimal_point(value):
 @register.filter
 def plus_value(value, number):
     return value + number
+
+
+@register.filter
+def polish_letter(letter):
+    polish = {
+        "A_": "Ą",
+        "E_": "Ę",
+        "C_": "Ć",
+        "O_": "Ó",
+        "S_": "Ś",
+        "Z_": "Ź",
+        "Z__" "Ż"
+    }
+    if polish.get(letter):
+        return polish.get(letter)
+    return letter
+    
